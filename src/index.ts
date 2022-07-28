a505a4ac8cbc003d79f016bf0f0d09eafbe60d67
Andrius Mozūraitis <andriusmozuraitis@gmail.com>
2022-07-28 13:18:24 +0300

    Improve database integrity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export * from './sample'
+import { Client } from 'graphql-ws'
+import { GraphQLClient } from 'graphql-request'
+import { Observable } from 'rxjs'
+import { DocumentNode, Kind, print } from 'graphql'
+function operationType(data: DocumentNode) {
+	for (const def of data.definitions) {
+		if (def.kind === Kind.OPERATION_DEFINITION) {
+			return def.operation
+		}
+	}
+	return
+}
+
+export type Requester<C = {}> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R> & Observable<R>
+export function createRequester(options: { websocket?: Client; request?: GraphQLClient }): Requester {
+	return (query, vars) => {
+		const op = operationType(query)
+		if (op === 'subscription') {
+			const ws = options.websocket
+			if (ws == null) throw new Error('Websocket Client is not provided!')
+			return new Observable<any>((observer) => {
+				return ws.subscribe(
+					{ query: print(query), variables: vars as any },
+					{
+						next: (c) => {
+							observer.next(c.data)
+						},
+						complete: () => {
+							observer.complete()
+						},
+						error: (err) => {
+							observer.error(err)
+						},
+					}
+				)
+			}) as any
+		}
+		const req = options.request
+		if (req == null) throw new Error('GraphQLClient is not provided!')
+
+		return req.rawRequest(print(query), vars).then((q) => q.data)
+	}
+}
