409f32ebd822fe310fdb57039487648a77886ce5
Andrius Mozūraitis <andriusmozuraitis@gmail.com>
2022-09-21 21:41:21 +0300

    Improve items

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Client } from 'graphql-ws'
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient, Variables } from 'graphql-request'
 import { Observable } from 'rxjs'
 import { DocumentNode, Kind, print } from 'graphql'
 import { createClient } from 'graphql-ws'
@@ -17,15 +17,16 @@ export type Requester<C = {}> = <R, V>(doc: DocumentNode, vars?: V, options?: C)
 export function easySetupRequester(args: {
 	ws?: { url: string; wsImplementation?: unknown }
 	request?: { url: string }
-	headers: Promise<() => Record<string, string>>
-}) {
+	headers: () => Promise<Record<string, string>>
+}): Requester {
 	const wsClient = args.ws
 		? createClient({
 				webSocketImpl: args.ws.wsImplementation,
 				url: args.ws.url,
 				connectionParams: async () => {
+					const headers = await args.headers()
 					return {
-						headers: args.headers,
+						headers,
 					}
 				},
 		  })
@@ -33,14 +34,40 @@ export function easySetupRequester(args: {
 
 	const makeRequestClient = () => {
 		if (args.request == null) return
-		return new GraphQLClient(args.request.url, { headers: args.headers as any })
+		return new GraphQLClient(args.request.url)
 	}
 	const requestClient = makeRequestClient()
 
-	return createRequester({
-		websocket: wsClient,
-		request: requestClient,
-	})
+	return (query, vars) => {
+		const op = operationType(query)
+		if (op === 'subscription') {
+			const ws = wsClient
+			if (ws == null) throw new Error('Websocket Client is not provided!')
+			return new Observable<any>((observer) => {
+				return ws.subscribe(
+					{ query: print(query), variables: vars as any },
+					{
+						next: (c) => {
+							observer.next(c.data)
+						},
+						complete: () => {
+							observer.complete()
+						},
+						error: (err) => {
+							observer.error(err)
+						},
+					}
+				)
+			}) as any
+		}
+		if (requestClient == null) throw new Error('GraphQLClient is not provided!')
+
+		const requestWithHeaders = async () => {
+			const headers = await args.headers()
+			return requestClient.request(query, vars, headers).then((q) => q.data)
+		}
+		return requestWithHeaders()
+	}
 }
 
 export function createRequester(options: { websocket?: Client; request?: GraphQLClient }): Requester {
